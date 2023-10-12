b2731cc071fd636f29ecab02214554e23dffeaaf
xian <xianirabernardo@gmail.com>
2023-10-13 00:41:55 +0800

    Installed React on project

---
+++ ribbit/src/pages/Home.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div class="container">
+        <div id="empty"></div>
+        <div id="post">
+          <div id="content">
+            <div id="postedBy">Posted by #DLSUFW # 23446</div>
+            <div id="message">
+              i checked my bfs phone and saw sa chat na he complimented this
+              girl with her new look lmao is that ok?
+            </div>
+            <div id="comments">Comments</div>
+          </div>
+        </div>
+        <div id="popular">
+          <div id="content">
+            <div id="postedBy">Posted by #DLSUFW # 23446</div>
+            <div id="message">
+              i checked my bfs phone and saw sa chat na he complimented this
+              girl with her new look lmao is that ok?
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
