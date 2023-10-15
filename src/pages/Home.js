c5687b46ee416c2aee79d10a71a4a8e1d5a4249c
xian <xianirabernardo@gmail.com>
2023-10-15 21:09:31 +0800

    sample posts

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,61 @@
 import React from "react";
+import { useState } from "react";
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div class="container">
-        <div id="empty"></div>
-        <div id="post">
+const Home = () => {
+  const [posts] = useState([
+    {
+      postedBy: "Posted by #DLSUFW # 23446",
+      message:
+        "i checked my bfs phone and saw sa chat na he complimented this girl with her new look lmao is that ok?",
+      comments: "comments",
+      id: 1,
+    },
+
+    {
+      postedBy: "Posted by #DLSUFW # 23447",
+      message: "hello world",
+      comments: "comments",
+      id: 2,
+    },
+
+    {
+      postedBy: "Posted by #DLSUFW # 23447",
+      message: "hello asdlf;jasdlf",
+      comments: "comments",
+      id: 3,
+    },
+
+    {
+      postedBy: "Posted by #DLSUFW # 23447",
+      message: "asdffffhjlsadgbflksj",
+      comments: "comments",
+      id: 4,
+    },
+  ]);
+
+  return (
+    <div className="container">
+      <div id="empty"></div>
+      {posts.map((post) => (
+        <div id="post" key={post.id}>
           <div id="content">
-            <div id="postedBy">Posted by #DLSUFW # 23446</div>
-            <div id="message">
-              i checked my bfs phone and saw sa chat na he complimented this
-              girl with her new look lmao is that ok?
-            </div>
-            <div id="comments">Comments</div>
+            <div id="postedBy">{post.postedBy}</div>
+            <div id="message">{post.message}</div>
+            <div id="comments">{post.comments}</div>
           </div>
         </div>
-        <div id="popular">
-          <div id="content">
-            <div id="postedBy">Posted by #DLSUFW # 23446</div>
-            <div id="message">
-              i checked my bfs phone and saw sa chat na he complimented this
-              girl with her new look lmao is that ok?
-            </div>
+      ))}
+      <div id="popular">
+        <div id="content">
+          <div id="postedBy">Posted by #DLSUFW # 23446</div>
+          <div id="message">
+            i checked my bfs phone and saw sa chat na he complimented this girl
+            with her new look lmao is that ok?
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Home;
