6070fe68a45664a01930a564dfe0ce6c3dc385a7
Aila <aila.janelle@yahoo.com>
2023-10-23 02:52:10 +0800

    fixed registration confirmation

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -5,27 +5,27 @@ const Confirmation = () => {
   return (
     <div className="container-fluid">
       <div className="row p-5">
-        <div className="col-7 d-flex flex-column align-content-center justify-content-center" />
-        <div className="welcome">Welcome to Ribbit!</div>
-        <div className="welDisc">
-          Ribbit is a General Community something blabla enter <br />
+        <div className="col-7 align-content-center justify-content-center mt-lg-5">
+          <div className="welcome">Welcome to Ribbit!</div>
+          <div className="welDisc">Ribbit is a General Community something blabla enter <br />
           some other dummy text i dont know what to write
-        </div>
-      </div>
-
-      <div className="col-5 d-flex flex-column justify-content-center align-items-center">
-        <div className="Confirm">
-          <div className="mx-auto spritesheet checkMark"></div>
-          <div className="TyForReg">Thank you for registering!</div>
-          <div className="d-flex  justify-content-center mt-4">
-            <div id="goodToGo">You're good to go!</div>
           </div>
-          <NavLink
-            to="/login"
-            className="d-flex  justify-content-center mt-4"
-            id="backToLogin">
-            Go to Login
-          </NavLink>
+        </div>
+
+        <div className="col-5 d-flex flex-column justify-content-center align-items-center">
+          <div className="Confirm">
+            <div className="mx-auto spritesheet checkMark"></div>
+            <div className="TyForReg">Thank you for registering!</div>
+            <div className="d-flex  justify-content-center mt-4">
+              <div id="goodToGo">You're good to go!</div>
+            </div>
+            <NavLink
+              to="/login"
+              className="d-flex  justify-content-center mt-4"
+              id="backToLogin">
+              Go to Login
+            </NavLink>
+          </div>
         </div>
       </div>
     </div>
