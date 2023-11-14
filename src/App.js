98b8df50a4a9828426d2aa03595bbbd5cfe27e35
xian <xianirabernardo@gmail.com>
2023-11-14 20:19:18 +0800

    edited folder structure

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
+import Home from "./components/Home";
+import Login from "./components/Login";
 import Header from "./components/Header";
 import HeaderAlt from "./components/HeaderAlt";
-import Register from "./pages/Register";
+import Register from "./components/Register";
 import Confirmation from "./components/Confirmation";
-import Profile from "./pages/Profile";
-import EditProfile from "./pages/EditProfile";
-import ViewPost from "./pages/ViewPost";
+import Profile from "./components/Profile";
+import EditProfile from "./components/EditProfile";
+import ViewPost from "./components/ViewPost";
 import comments from "./components/Comments";
-import SearchComment from "./pages/SearchComment";
-import SearchPost from "./pages/SearchPost";
+import SearchComment from "./components/SearchComment";
+import SearchPost from "./components/SearchPost";
 import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { db } from "./config/firebase";
@@ -26,12 +26,17 @@ function App() {
   useEffect(() => {
     const getPosts = async () => {
       try {
+        // Get the data from the database
         const data = await getDocs(postsCollectionRef);
+
+        // Convert the data to the format we want
         const posts = data.docs.map((doc) => ({
           ...doc.data(),
           id: doc.id,
           postTime: doc.data().postTime.toDate().toDateString(),
         }));
+
+        // Set the state of the posts array to the data from the database
         setPost(posts);
       } catch (error) {
         console.log(error);
