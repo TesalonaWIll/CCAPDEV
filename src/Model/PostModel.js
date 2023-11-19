fa70342c38b3903ac856754d4bef02dde3823686
xian <xianirabernardo@gmail.com>
2023-11-19 16:30:28 +0800

    Fixed MVC format

--- src/Model/PostModel.js
+++ src/Model/PostModel.js
@@ -1,13 +1,78 @@
-// PostModel.js
-import { db } from "./config/firebase";
-import { getDocs, collection } from "firebase/firestore";
+import { db } from "./firebase";
+import {
+  doc,
+  getDoc,
+  getDocs,
+  updateDoc,
+  collection,
+  addDoc,
+  query,
+  where,
+} from "firebase/firestore";
+
+export const fetchUserFromDatabase = async (userID) => {
+  const usersCollectionRef = collection(db, "users");
+  const userDoc = doc(usersCollectionRef, userID);
+  const userSnapshot = await getDoc(userDoc);
+
+  if (userSnapshot.exists()) {
+    return { id: userSnapshot.id, ...userSnapshot.data() };
+  } else {
+    console.log("No such user!");
+    return null;
+  }
+};
 
 export const fetchPostsFromDatabase = async () => {
   const postsCollectionRef = collection(db, "posts");
   const data = await getDocs(postsCollectionRef);
+
   return data.docs.map((doc) => ({
     ...doc.data(),
     id: doc.id,
     postTime: doc.data().postTime.toDate().toDateString(),
   }));
+};
+
+export const fetchCommentsFromDatabase = async (postID) => {
+  const commentsCollectionRef = collection(db, "comments");
+  const q = query(commentsCollectionRef, where("postID", "==", postID));
+  const data = await getDocs(q);
+  return data.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+};
+
+export const fetchCategoriesFromDatabase = async () => {
+  const categoriesCollectionRef = collection(db, "categories");
+  const data = await getDocs(categoriesCollectionRef);
+
+  return data.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+};
+
+export const addPostToDatabase = async (post) => {
+  const postsCollectionRef = collection(db, "posts");
+  await addDoc(postsCollectionRef, post);
+};
+
+export const addCommentToDatabase = async (commentText, postID) => {
+  const commentsCollectionRef = collection(db, "comments");
+  const newComment = { ...comment, commentContent: commentText, postID };
+  await addDoc(commentsCollectionRef, newComment);
+};
+
+export const updatePostInDatabase = async (post) => {
+  const postsCollectionRef = collection(db, "posts");
+  const postDoc = doc(postsCollectionRef, post.id);
+  await updateDoc(postDoc, post);
+};
+
+export const updateCommentInDatabase = async (comment) => {
+  const commentsCollectionRef = collection(db, "comments");
+  const commentDoc = doc(commentsCollectionRef, comment.id);
+  await updateDoc(commentDoc, comment);
 };
--- src/controller/PostController.js
+++ src/controller/PostController.js
@@ -1,5 +1,24 @@
-// PostController.js
-import { fetchPostsFromDatabase } from "../Model/PostModel";
+import {
+  fetchUserFromDatabase,
+  fetchPostsFromDatabase,
+  fetchCommentsFromDatabase,
+} from "../Model/PostModel";
+import {
+  addPostToDatabase,
+  addCommentToDatabase,
+  updatePostInDatabase,
+  updateCommentInDatabase,
+} from "../Model/PostModel";
+
+export const getUser = async (userID) => {
+  try {
+    const user = await fetchUserFromDatabase(userID);
+    return user;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
 
 export const getPosts = async () => {
   try {
@@ -9,4 +28,126 @@ export const getPosts = async () => {
     console.error(error);
     return [];
   }
+};
+
+export const getCurrentPost = async (postID) => {
+  try {
+    const posts = await fetchPostsFromDatabase();
+    const currentPost = posts.find((post) => post.id === postID);
+    return currentPost;
+  } catch (error) {
+    console.error(error);
+    return {};
+  }
+};
+
+export const getComments = async (postID) => {
+  try {
+    const comments = await fetchCommentsFromDatabase(postID);
+    return comments;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+export const addPost = async (post) => {
+  try {
+    await addPostToDatabase(post);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const addComment = async (comment, postID) => {
+  try {
+    await addCommentToDatabase(comment, postID);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const updatePost = async (post) => {
+  try {
+    await updatePostInDatabase(post);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const handleUpvote = async (postID, userID) => {
+  try {
+    let posts = await getPosts();
+    posts.map((post) => {
+      if (post.id === postID) {
+        post.upvoted = !post.upvoted;
+        post.downvoted = false;
+        post.upvotedBy.push(userID) && post.downvotedBy.pop(userID);
+        updatePostInDatabase(this.post);
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const handleDownvote = async (postID, userID) => {
+  try {
+    let posts = await getPosts();
+    posts.map((post) => {
+      if (post.id === postID) {
+        post.downvoted = !post.downvoted;
+        post.upvoted = false;
+        post.downvotedBy.push(userID) && post.upvotedBy.pop(userID);
+        updatePostInDatabase(this.post);
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const handleUpvoteComment = async (commentID, userID) => {
+  try {
+    let comments = await getComments();
+    comments.map((comment) => {
+      if (comment.id === commentID) {
+        comment.upvoted = !comment.upvoted;
+        comment.downvoted = false;
+        comment.upvotedBy.push(userID) && comment.downvotedBy.pop(userID);
+        updateCommentInDatabase(this.comment);
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const handleDownvoteComment = async (commentID, userID) => {
+  try {
+    let comments = await getComments();
+    comments.map((comment) => {
+      if (comment.id === commentID) {
+        comment.downvoted = !comment.downvoted;
+        comment.upvoted = false;
+        comment.downvotedBy.push(userID) && comment.upvotedBy.pop(userID);
+        updateCommentInDatabase(this.comment);
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const handleCommentChange = (event, setCommentText) => {
+  setCommentText(event.target.value);
+  console.log(event.target.value);
+};
+
+export const handleCommentSubmit = (
+  postID,
+  setCommentText,
+) => {
+  addCommentToDatabase(setCommentText, postID);
+  setCommentText("");
 };
