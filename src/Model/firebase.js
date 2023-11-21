d5f119dd179ea0a09c6c2449b9ff02ea51727280
xian <xianirabernardo@gmail.com>
2023-11-21 15:13:45 +0800

    hid api keys

--- src/Model/firebase.js
+++ src/Model/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAqyoufAadauvaoYho1j04yPnhE3K4Ug1g",
-  authDomain: "ribbit-website.firebaseapp.com",
-  projectId: "ribbit-website",
-  storageBucket: "ribbit-website.appspot.com",
-  messagingSenderId: "46177706288",
-  appId: "1:46177706288:web:1aa56379c1728caebe68f5",
-  measurementId: "G-J0HHJ2PFQR",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
